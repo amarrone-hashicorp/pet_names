a0f5f1652304ca83f25a512b60ce91646aa7dc85
amarrone-hashicorp <86081038+amarrone-hashicorp@users.noreply.github.com>
2022-01-05 11:51:24 -0800

    created new random string

--- provider.tf
+++ provider.tf
@@ -1,9 +1,9 @@
 terraform {
-required_providers {
-random = {
-source = "hashicorp/random"
-version >= "2.3.0"
-}
-}
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "3.1.0"
+    }
+  }
 }
 
